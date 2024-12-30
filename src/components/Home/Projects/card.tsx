c78c2eaa923785ecfc1a4fd401768f3ebd02c570
Imran Ahmed <imran.ahk26@gmail.com>
2024-12-30 08:49:42 +0600

    Update: Project responsive

--- src/components/Home/Projects/card.tsx
+++ src/components/Home/Projects/card.tsx
@@ -20,25 +20,27 @@ export function Card({
 
   return (
     <motion.div
-      className='flex flex- h-[60dvh] 2xl:h-[55dvh] overflow-hidden shadow-lg rounded-3xl '
+      className='flex flex-col md:flex-row h-[60dvh] 2xl:h-[55dvh] overflow-hidden shadow-lg rounded-large md:rounded-3xl '
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -50 }}
       transition={{ duration: 0.9, ease: 'easeOut' }}
       key={index}
     >
-      <div className='p-6 flex flex-col w-1/2 flex-grow'>
-        <p className='mb-4 text-xl drop-shadow-lg'>{type}</p>
-        <h2 className='text-2xl lg:text-4xl font-bold drop-shadow mb-2 pt-5'>
+      <div className='p-6 gap-1 flex flex-col md:w-1/2 flex-grow order-2 md:order-1'>
+        <p className='md:mb-4 md:text-xl drop-shadow-lg'>{type}</p>
+        <h2 className='text-xl lg:text-4xl font-bold drop-shadow md:mb-2 pt-2 md:pt-5'>
           {title}
         </h2>
-        <p className='mb-4 text-xl font-semibold text-brandLight dark:text-brandDark'>
+        <p className=' md:mb-4 md:text-xl font-semibold text-brandLight dark:text-brandDark'>
           {subTitle}
         </p>
-        <p className='mb-4 text-gray-500 dark:text-gray-400'>{description}</p>
+        <p className='md:mb-4 text-xs text-gray-500 dark:text-gray-400 max-h-24 overflow-hidden'>
+          {description}
+        </p>
         {link && (
           <p
-            className='mb-4 lg:mt-6 text-brandDark cursor-pointer'
+            className='mb- lg:mt-6 text-brandDark cursor-pointer max-w-min'
             onClick={() => window.open(link, '_blank', 'noopener,noreferrer')}
           >
             {link?.split('https://')[1] || ''}
@@ -46,11 +48,11 @@ export function Card({
         )}
       </div>
       <div
-        className={`relative w-1/2 overflow-hidden p-10 ${projectColor} flex flex-col justify-end`}
+        className={`relative md:w-1/2 overflow-hidden md:p-10 ${projectColor} md:flex flex-col justify-end order-1 md:order-2`}
       >
         {/* First Image (Initially behind) */}
         <motion.div
-          className='absolute top-9 right-16 w-4/6 z-0'
+          className='absolute top-9 right-16 w-4/6 z-0 hidden md:block'
           initial={{ scale: 1, zIndex: 0 }}
           whileHover={{
             scale: 1.1,
@@ -63,14 +65,14 @@ export function Card({
 
         {/* Second Image (Initially in front) */}
         <motion.div
-          className='relative z-10'
+          className='md:relative z-10'
           initial={{ scale: 1, zIndex: 10 }}
           whileHover={{
             scale: 1.05,
             transition: { duration: 0.3 },
           }}
         >
-          <Image src={imagePrimary} alt={title} className='rounded-large' />
+          <Image src={imagePrimary} alt={title} className='md:rounded-large' />
         </motion.div>
       </div>
     </motion.div>
--- src/components/Home/Projects/index.tsx
+++ src/components/Home/Projects/index.tsx
@@ -1,20 +1,38 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from '@/components/Interface/CustomFeature/Carousal';
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 const Projects = () => {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
+  const isDark = resolvedTheme === 'dark';
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return <div>[]</div>;
+
   return (
     <div className='flex flex-col justify-center items-center pt-20'>
       <div className='max-w-7xl flex flex-col items-center'>
         <div className=''>
-          <Image
-            src={'/assets/girl-programmer.png'}
-            alt='dev'
-            width={600}
-            height={300}
-            className=''
-          />
+          {!isDark ? (
+            <Image
+              src={'/assets/girl-programmer.png'}
+              alt='dev'
+              width={600}
+              height={300}
+              className=''
+            />
+          ) : (
+            <Image
+              src={'/assets/girl-programmer-dark.png'}
+              alt='dev'
+              width={600}
+              height={300}
+              className=''
+            />
+          )}
         </div>
         <Carousel />
       </div>
