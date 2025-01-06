a86a086ea66ff9ddbb38ad06a0e78a090ac918f7
Imran Ahmed <imran.ahk26@gmail.com>
2025-01-06 12:38:26 +0600

    Add: About page

--- src/lib/helper/index.tsx
+++ src/lib/helper/index.tsx
@@ -1,6 +1,13 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export const colorClasses: Record<string, string> = {
   green: 'bg-green-200/40',
   orange: 'bg-amber-200/40',
   blue: 'bg-indigo-200/40',
   red: 'bg-red-300/40',
 }; // Remember include these colors in tailwind config safelist
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
